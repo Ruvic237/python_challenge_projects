112e38e094c5d0d32bf36f362fa0ef2c4c5ad6d5
Ruvic237 <bambavarnes@gmail.com>
2023-05-24 02:00:11 +0200

    Day one task

---
+++ day1_task/bandname.py
@@ -0,0 +1,12 @@
+# This is a bandname generator
+
+print("Welcome to the bandname Generator.")
+
+# obtain the city of the user
+city = input("Which city are you from? \n")
+
+# obtain the name of the favorite user pet
+user_pet = input("What the name of your favorite pet? \n")
+
+# displaying a bandname to the user base on city and user_pet
+print("Dear User, your bandname could be {} {}".format(city,user_pet))
---
+++ day1_task/switch.py
@@ -0,0 +1,16 @@
+# A program to collect and switch numbers entered by the user
+
+# obtaining value of a and b
+a = input("a:")
+b = input("b:")
+
+# switching the values
+hold_a = a
+
+a = b
+b = hold_a
+
+# displaying how the values have been switched
+print("a = " + a)
+print("b = " + b)
+ 
